f0e201fe6eef594a375efa828ac75282f3959042
shawn-park <shawn.park@iumsoft.com>
2025-10-13 21:59:36 +0900

    no message

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "shawn-park.github.io/finance/",
+    baseUrl: "shawn-park.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
